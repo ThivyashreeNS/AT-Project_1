8330f7a8118e6ef84c039aa4a13201d443909657
ThivyashreeNS <nstshree2000@gmail.com>
2024-11-02 08:28:07 +0530

    Data.py

--- Data.py
+++ Data.py
@@ -3,15 +3,18 @@ Data.py
 This Python file holds basic data """
 
 class WebData:
+    # Login Data
     url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     dashboard_url = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
     excel_file = r"D:\Workspace\Python_Workspace\PAT\Selenium_1\AT_Project_1\test_data.xlsx"
     sheet_number = "DataSheet"
-
-    firstname = "Shree"
-    middlename = "T"
-    lastname = "NS"
+     
+    # Employee data
+    firstname = "Naru"
+    middlename = "to"
+    lastname = "Uzumaki"
     photo = r"D:\Workspace\Python_Workspace\PAT\Selenium_1\AT_Project_1\butterfly.jpeg"
 
+    # Employee details
     license_number = "1234567890"
 
